e341c16b42176f29b516f9fc8e92ba9c1d0fcd31
Eric Billings <billings.company@gmail.com>
2019-07-18 15:47:18 -0400

    pull gameWord from acctWords and split into array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,28 +2,49 @@
 
 const acctWords = ["profit", "asset", "revenue", "equity"];
 let guessRecord = [];
+let i = 0;
+let gameWord;
 
 
-document.onkeyup = function (event) {
-    const userGuess = event.key;
 
-    if (guessRecord.includes(userGuess)) {
-        alert("You already guessed that letter!");
+/*NOT WORKING
+let gameWord = (acctWords.indexOf[1]);*/
 
-    } else {
-
-        guessRecord.push(userGuess);
-
-        if (guessRecord.length < 16) {
+$("#random-button").on("click", function () {
 
 
-            console.log(acctWords);
-            console.log(userGuess);
-            console.log(guessRecord);
-            console.log(guessRecord.length);
+    gameWord = acctWords[i];
+    console.log(gameWord);
+    letterArray = gameWord.split("");
+
+
+    document.onkeyup = function (event) {
+        const userGuess = event.key;
+
+        if (guessRecord.includes(userGuess)) {
+            alert("You already guessed that letter!");
+
+        } else {
+
+            guessRecord.push(userGuess);
+
+            if (guessRecord.length < 16) {
+
+
+                console.log(userGuess);
+                console.log(guessRecord);
+                console.log(guessRecord.length);
+                console.log(gameWord);
+                console.log(letterArray);
+
+
+                
+
+            }
 
         }
 
     }
 
-};
+
+});
